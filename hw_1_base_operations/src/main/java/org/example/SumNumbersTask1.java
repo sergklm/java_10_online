e71282be4adde9f2a8f7033949026c3c3c8c564d
sergejklimenko <klimenko4595@gmail.com>
2024-01-12 18:58:59 +0200

    HW1

---
+++ hw_1_base_operations/src/main/java/org/example/SumNumbersTask1.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class SumNumbersTask1 {
+    public static void sumNumbers(BufferedReader reader) throws IOException {
+        System.out.print("Enter a value to calculate the sum of numbers: ");
+        String input = reader.readLine();
+
+        int sum = 0;
+        int currentNum = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+
+            if (Character.isDigit(c)) {
+                currentNum = currentNum * 10 + Character.getNumericValue(c);
+            } else if (currentNum > 0) {
+                sum += currentNum;
+                currentNum = 0;
+            }
+        }
+
+        if (currentNum > 0) {
+            sum += currentNum;
+        }
+
+        System.out.println("Sum of numbers: " + sum);
+    }
+}
---
+++ hw_1_base_operations/src/main/java/org/example/SymbolCounterTusk2.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class SymbolCounterTusk2 {
+    public static void countSymbols(BufferedReader reader) throws IOException {
+        System.out.print("Enter a value for the character count: ");
+        String input = reader.readLine();
+
+        int[] latinAndCyrillic = new int[128];
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= 'а' && c <= 'я') ||
+                    (c >= 'А' && c <= 'Я')) {
+                latinAndCyrillic[c]++;
+            }
+        }
+
+        for (int i = 0; i < 128; i++) {
+            if (latinAndCyrillic[i] > 0) {
+                char currChar = (char) i;
+                System.out.println((i + 1) + ". " + currChar + " - " + latinAndCyrillic[i]);
+            }
+        }
+    }
+}
